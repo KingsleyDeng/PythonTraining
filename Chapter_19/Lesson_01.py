e2dc44b80767b6023ad9259cef145805bb9563fc
KingsleyDeng <460556933@qq.com>
2020-02-26 17:17:38 +0800

    Python疯狂讲义练习

---
+++ Chapter_19/Lesson_01.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+x_data = ['2011', '2012', '2013', '2014', '2015', '2016']
+y_data = [58000, 64000, 23000, 43341, 34523, 70000]
+
+plt.plot(x_data, y_data)
+plt.show()
